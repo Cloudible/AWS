21baa08a1bf26a746f96e739aa61a374df6d7d35
Gjuni <kmj06178@gmail.com>
2025-07-29 20:30:43 +0900

    feat: iam function added

--- src/router.Command/router.ts
+++ src/router.Command/router.ts
@@ -1,7 +1,8 @@
 import { awsCommand } from "../command/AWS.command";
-import { ec2Command } from "../command/EC2.command";
-import { rdsCommand } from "../command/RDS.command";
+// import { ec2Command } from "../command/EC2.command";
+// import { rdsCommand } from "../command/RDS.command";
 
-const commandRouter = [awsCommand, ec2Command, rdsCommand];
+// const commandRouter = [awsCommand, ec2Command, rdsCommand];
+const commandRouter = [awsCommand];
 
 export default commandRouter;
