b4fee8e85590b62fab15aea775b3e82cb7f355d2
Gjuni <kmj06178@gmail.com>
2025-07-29 21:23:48 +0900

    fix: account overwritten

--- dist/user/user.Controller/user.Controller.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.postUserInfoController = void 0;
-const postUserInfoController = async (req, res, next) => {
-    const info = req.body;
-};
-exports.postUserInfoController = postUserInfoController;
