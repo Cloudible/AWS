6b75af024bf3bf78e498556adc5b7eb66f33a0f3
Gjuni <kmj06178@gmail.com>
2025-09-03 15:01:56 +0900

    express Router removed

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,8 @@
-import express from 'express';
 import dotenv from 'dotenv';
-import cors from 'cors';
-import morgan from 'morgan';
 import startDiscordBot from './config/discord';
 
-const app = express();
-const router = express.Router();
 dotenv.config();
 const port = process.env.PORT;
 
-
-app.use(cors());
-app.use(express.json());
-app.use(morgan('dev'));
-
 // discord bot opperator    
 startDiscordBot();
-
-router.get('/', (req, res) => {
-    res.json('aws discord bot');
-});
-
-// app.use('/user', userRouter);
-
-app.listen(port, () => {
-    console.log(`Server is Running on port ${port}`);
-});
